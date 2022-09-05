569d96b61a3a3ffbf1007e7e22f1962a45a94bc1
Vkonyushevsky <vkonyushevsky@gmail.com>
2022-09-05 19:33:16 +0300

    test

---
+++ src/com/epam/rd/autotasks/meetautocode/HelloAutocode.java
@@ -0,0 +1,11 @@
+package com.epam.rd.autotasks.meetautocode;
+
+public class HelloAutocode {
+    public static void main(String[] args) {
+        System.out.println("Hello, Autocode!");
+
+    }
+}
+
+
+
